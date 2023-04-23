3031bd4ba71303934df5d00739d2fdb273bcddca
Javier Hernandez Gonzalez <86983739+Arguish@users.noreply.github.com>
2023-04-23 22:21:53 +0100

    local storage

--- src/Context/Context/ContextProvider.jsx
+++ src/Context/Context/ContextProvider.jsx
@@ -1,7 +1,23 @@
 import { GlobalContext } from "./Context";
 import { useState } from "react";
 
+//Imports ^^^^^^^^^
+/////////////////
+const TEMPLATEPLAYER = {
+  id: "User",
+  type: "Player",
+};
+/////////////////
+//Componet exported VVVVVVVVVV
+
 function ContextProvider({ children }) {
+  localStorage.setItem("Arguish", JSON.stringify(TEMPLATEPLAYER));
+
+  console.log(localStorage.getItem("Arguish"));
+  console.log(Object.keys(localStorage));
+  console.log(Object.values(localStorage).map((a) => JSON.parse(a)));
+  localStorage.setItem("Arguish", JSON.stringify({ alo: "2" }));
+  console.log(Object.values(localStorage).map((a) => JSON.parse(a)));
   const meaning = 42;
   const [inventory, setinventory] = useState([]);
   const addToInventory = (item) => {
