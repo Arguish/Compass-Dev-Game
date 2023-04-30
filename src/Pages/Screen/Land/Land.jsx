4db199b1be1291e17b903707ae90f0b63970d3b1
Javier Hernandez Gonzalez <86983739+Arguish@users.noreply.github.com>
2023-04-30 16:29:57 +0100

    Layout update

--- src/Pages/Screen/Land/Land.jsx
+++ src/Pages/Screen/Land/Land.jsx
@@ -4,14 +4,30 @@ function Land() {
   return (
     <>
       <div style={{ height: "83vh" }} className="grid h-screen">
-        <div className="col-start-1 row-start-1 col-span-3 row-span-5">1</div>
-        <div className="col-start-4 row-start-1 col-span-1 row-span-1">2</div>
-        <div className="col-start-4 row-start-2 col-span-1 row-span-2">3</div>
-        <div className="col-start-4 row-start-4 col-span-1 row-span-2">4</div>
-        <div className="col-start-1 row-start-6 col-span-1 row-span-4">5</div>
-        <div className="col-start-2 row-start-6 col-span-3 row-span-2">6</div>
-        <div className="col-start-2 row-start-8 col-span-1 row-span-2">7</div>
-        <div className="col-start-3 row-start-8 col-span-2 row-span-2">8</div>
+        <div className="col-start-1 row-start-1 col-span-3 row-span-5">
+          Mision principal
+        </div>
+        <div className="col-start-4 row-start-1 col-span-1 row-span-1">
+          Recursos pasivos
+        </div>
+        <div className="col-start-4 row-start-2 col-span-1 row-span-2">
+          Tienda Generica
+        </div>
+        <div className="col-start-4 row-start-4 col-span-1 row-span-2">
+          Tienda especial
+        </div>
+        <div className="col-start-1 row-start-6 col-span-1 row-span-4">
+          Dungeon
+        </div>
+        <div className="col-start-2 row-start-6 col-span-3 row-span-2">
+          Mision Secundaria
+        </div>
+        <div className="col-start-2 row-start-8 col-span-1 row-span-2">
+          ??????
+        </div>
+        <div className="col-start-3 row-start-8 col-span-2 row-span-2">
+          Arena?
+        </div>
       </div>
     </>
   );
--- src/Pages/Screen/Mission/Mission.jsx
+++ src/Pages/Screen/Mission/Mission.jsx
@@ -4,12 +4,24 @@ function Mission() {
   return (
     <>
       <div style={{ height: "83vh" }} className="grid h-screen">
-        <div className="col-start-1 row-start-1 col-span-4 row-span-2">1</div>
-        <div className="col-start-1 row-start-3 col-span-4 row-span-1">2</div>
-        <div className="col-start-3 row-start-4 col-span-2 row-span-2">3</div>
-        <div className="col-start-1 row-start-4 col-span-2 row-span-5">4</div>
-        <div className="col-start-3 row-start-6 col-span-2 row-span-1">5</div>
-        <div className="col-start-3 row-start-7 col-span-2 row-span-2">6</div>
+        <div className="col-start-1 row-start-1 col-span-4 row-span-2">
+          Titulo y descripcion
+        </div>
+        <div className="col-start-1 row-start-3 col-span-4 row-span-1">
+          Posibles enemigos
+        </div>
+        <div className="col-start-3 row-start-4 col-span-2 row-span-2">
+          recursos que se pueden conseguir
+        </div>
+        <div className="col-start-1 row-start-4 col-span-2 row-span-5">
+          Personaje al que puedes enviar
+        </div>
+        <div className="col-start-3 row-start-6 col-span-2 row-span-1">
+          hueco vacio, mensaje
+        </div>
+        <div className="col-start-3 row-start-7 col-span-2 row-span-2">
+          Boton de ir
+        </div>
       </div>
     </>
   );
