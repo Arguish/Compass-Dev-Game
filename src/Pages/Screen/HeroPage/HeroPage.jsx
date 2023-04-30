938676acfc6c5578524370a863852f80fb8a6dd1
Javier Hernandez Gonzalez <86983739+Arguish@users.noreply.github.com>
2023-04-30 14:09:37 +0100

    hero page ajustado

--- src/Pages/Screen/HeroPage/HeroPage.jsx
+++ src/Pages/Screen/HeroPage/HeroPage.jsx
@@ -6,7 +6,10 @@ function HeroPage() {
   const { ActiveHero, setActiveHero } = useContext(GlobalContext);
   return (
     <>
-      <div className="grid grid-col-4 grid-row-8 h-screen ">
+      <div
+        style={{ height: "83vh" }}
+        className="grid grid-col-4 grid-row-8 h-screen"
+      >
         <div className="col-start-1 h-1/8">Abalorio 1</div>
         <div className="col-start-2 row-span-2 h-2/8">Arma 2</div>
         <div className="col-start-3 row-span-2 h-2/8">Arma 3</div>
@@ -18,9 +21,14 @@ function HeroPage() {
         </div>
         <div className="col-start-1 row-start-4 row-span-2 h-2/8">Troso</div>
         <div className="col-start-4 row-start-4 row-span-2 h-2/8">Piernas</div>
-        <div className="col-start-2 row-start-5 col-span-2 row-span-2 h-2/8">
-          Hombreras
+        <div className="col-start-2 row-start-5 col-span-1 row-span-2 h-2/8">
+          Casco
         </div>
+        <div className="col-start-3 row-start-5 col-span-1 row-span-2 h-2/8">
+          Botas
+        </div>
+        <div className="col-start-1 h-1/8">Abalorio 3</div>
+        <div className="col-start-4 h-1/8">Abalorio 4</div>
       </div>
     </>
   );
